d9d97f53eb026f63fa1786887cb4fabcd735ba74
Alexander Tarasenko <alexander.tarasenko@namecheap.com>
2021-04-10 15:26:55 +0300

    fix: menu close

--- src/pages/header/MenuDropdowns.tsx
+++ src/pages/header/MenuDropdowns.tsx
@@ -18,7 +18,7 @@ export const MenuDropdowns: React.FC<IMenuDropdowns> = ({dropdownClosing, openDr
         openDropdown();
     };
 
-    const mainWaysJSX = mainWaysData.map((child: Children) => <Link to={child.link} className='col-3'> <h6 className="card-header-text-style">{child.title}</h6> </Link>);
+    const mainWaysJSX = mainWaysData.map((child: Children) => <Link to={child.link} className='col-3' onClick={handleLinkSelection}> <h6 className="card-header-text-style">{child.title}</h6> </Link>);
 
     return (
        menuItem === 1 ? <div className='mt-2'>
