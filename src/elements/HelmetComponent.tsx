0388a841508207e0002007ddac1394df3d768703
Alexander Tarasenko <alexander.tarasenko@namecheap.com>
2021-09-26 20:56:10 +0300

    fix: add all paths to react-snap

--- src/elements/HelmetComponent.tsx
+++ src/elements/HelmetComponent.tsx
@@ -6,17 +6,15 @@ interface HelmetComponentProps {
 }
 
 export const HelmetComponent: React.FC<HelmetComponentProps> = ({data}) => {
+  let text = data.manufacturers ? data.manufacturers.text : data.underTitle ? data.underTitle : data.firstText ? data.firstText.text : data.secondText ? data.secondText.text : data.text ? data.text : 'Ремонт и техническое обслуживание | Работы по монтажу и демонтажу | Изготовление металлоконструкций | Строительные работы и др.';
 
+  text  = text ?? 'Ремонт и техническое обслуживание | Работы по монтажу и демонтажу | Изготовление металлоконструкций | Строительные работы и др.';
   return (
-        <Helmet>
-            <title>{data.title} | Ситам</title>
-            <meta name="description" content={`${data.manufacturers ? data.manufacturers.text.slice(0, 296) :
-                data.underTitle ? data.underTitle.slice(0, 296) :
-                    data.firstText ? data.firstText.text.replace(/#/g, '').slice(0, 296) :
-                        data.secondText ? data.secondText.text.replace(/#/g, '').slice(0, 296):
-                            data.text.replace(/#/g, '').slice(0, 296)}...`} />
-            <link rel="canonical" href={`https://sitam.tech${data.url}`}/>
-            {data.keywords && <meta name="keywords" content={data.keywords} />}
-        </Helmet>
+    <Helmet>
+        <title>{data.title} | Ситам</title>
+        <meta name="description" content={`${text.trim().replace(/#/g, '').replace(/\\n?\\r/, '').slice(0, 296)}...`} />
+        <link rel="canonical" href={`https://sitam.tech${data.url}`}/>
+        {data.keywords && <meta name="keywords" content={data.keywords} />}
+    </Helmet>
   );
 };
