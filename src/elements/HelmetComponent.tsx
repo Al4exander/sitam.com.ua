04527ba8fcde391c1753388b19d5cda3c5cab60c
Alexander Tarasenko <alexander.tarasenko@namecheap.com>
2021-10-16 20:24:25 +0300

    fix: change canonical

--- src/elements/HelmetComponent.tsx
+++ src/elements/HelmetComponent.tsx
@@ -12,7 +12,7 @@ export const HelmetComponent: React.FC<HelmetComponentProps> = ({data}) => {
     <Helmet>
         <title>{data.title} | Ситам</title>
         <meta name="description" content={`${text.trim().replace(/#/g, '').replace(/\\n?\\r/g, '').slice(0, 300)}...`} />
-        <link rel="canonical" href={`https://sitam.tech${data.url}`}/>
+        <link rel="canonical" href={`https://sitam.tech${data.url}/`}/>
         {data.keywords && <meta name="keywords" content={data.keywords} />}
     </Helmet>
   );
