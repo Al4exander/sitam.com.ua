fcbb75b252ab605b70e63b8bfed344347a35282c
alexandertarasenko <alexander.tarasenko@namecheap.com>
2024-04-30 12:01:00 +0300

    feat: add language selector

--- src/pages/standartPages/commonElements/Manufacturers.tsx
+++ src/pages/standartPages/commonElements/Manufacturers.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import StandardManufacturers from './../../../images/pages/standardManufacturers/StandardManufacturers.png';
 import StandardManufacturers2 from './../../../images/pages/standardManufacturers/StandardManufacturers2.png';
 
 import './styles/Manufacturers.css';
+import {LanguageContext} from "../../../lib/languageContext";
 
 export const Manufacturers: React.FC<IManufacturers> = ({ children, className, text, images, title}) => {
+    const [language] = useContext(LanguageContext);
+    const alternateTitle = language === 'ua' ? 'Виробники' : 'Производители';
     return (
         <section className={`col-12 col-xl-10 col-lg-10 col-md-12 col-sm-12 mt-5 ${className}`}>
             <header className='manuf-header align-center-full'>
-                {title ? title : 'Производители'}
+                {title ? title : alternateTitle}
             </header>
             <span className='manuf-body mt-4 d-flex justify-content-center'>
                 {text}
