c0202e925d8d651630414b2bf1add3a239f60471
alexandertarasenko <alexander.tarasenko@namecheap.com>
2024-06-23 21:36:38 +0300

    feat: update social medias

--- src/pages/header/components/socialContacts/Icons.tsx
+++ src/pages/header/components/socialContacts/Icons.tsx
@@ -6,13 +6,13 @@ import whatsapp from '../socialContacts/images/whatsapp.png';
 export const Icons: React.FC<IIcons> = ({className, iconClassName}) => {
     return (
         <div className={className}>
-            <a className={iconClassName} target='_blank' rel='noopener noreferrer' href='viber://chat?number=380666951098' title={'Viber'}>
+            <a className={iconClassName} target='_blank' rel='noopener noreferrer' href='viber://chat?number=380676951098' title={'Viber'}>
                 <img className='header-icon-first' src={viber}  title={'Viber'} alt={'Viber'} loading='lazy'/>
             </a>
-            <a href='https://t.me/Sitamtech' target='_blank' rel='noopener noreferrer' className={iconClassName} title={'Telegram'}>
+            <a href='https://t.me/Sitam_LLC' target='_blank' rel='noopener noreferrer' className={iconClassName} title={'Telegram'}>
                 <img className='header-icon' src={telegram} title={'Telegram'} alt={'Telegram'} loading='lazy'/>
             </a>
-            <a className={iconClassName} target='_blank' rel='noopener noreferrer' href='https://wa.me/+380666951098?text=Доброго дня,%20я%20стосовно' title={'Whatsapp'} >
+            <a className={iconClassName} target='_blank' rel='noopener noreferrer' href='https://wa.me/+380676951098?text=Доброго дня,%20я%20стосовно' title={'Whatsapp'} >
                 <img className='header-icon' src={whatsapp} title={'Whatsapp'} alt={'Whatsapp'} loading='lazy'/>
             </a>
         </div>
