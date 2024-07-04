3e2a9009b7c3da2a3fc7bb2ab70cdc33b0d31aae
alexandertarasenko <alexander.tarasenko@namecheap.com>
2024-07-04 17:51:34 +0300

    feat: update terms and agreements, change colors

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -4,6 +4,8 @@ import {Icons} from "../header/components/socialContacts/Icons";
 import {LanguageContext} from "../../lib/languageContext";
 import {Link} from "react-router-dom";
 import {SizeContext} from "../../lib/sizeContext";
+import visaIcon from './images/visa.svg';
+import masterCardIcon from './images/mastercard.svg';
 import cx from "classnames";
 
 export function Footer() {
@@ -17,7 +19,7 @@ export function Footer() {
         privacy: 'Угода про передачу персональних данних',
         oferta: 'Договір публічної оферти',
         otkaz: 'Згода на обробку персональних данних',
-    }: {
+    } : {
         privacy: 'Соглашение про передачу персональных данных',
         oferta: 'Договор публичной оферты',
         otkaz: 'Соглашение на оброботку персональных данных',
@@ -49,69 +51,68 @@ export function Footer() {
 
     return (
         <div className={`sitam-footer${isHydrated ? '' : ' not-hydrated'}`}>
-            <div className={`mt-3 mb-3 text-center ${isHydrated ? '' : ' not-hydrated'}`}>
-                <div className='mt-3'>
-                    {
-                        Object.entries(address).map(([key, value]) => {
-                            return <span className='mr-3'
-                                         key={value}>
-                                {value}
-                            </span>
-                        })
-                    }
-                </div>
-                <div className='mt-3 d-flex flex-wrap justify-content-center'>
-                    {contactsName}
-                    {
-                        Object.entries(contacts).map(([key, value]) => {
-                            const phoneNumber = ['phoneNumber', 'extraPhoneNumber'];
-                            const email = ['email'];
-
-                            if (phoneNumber.includes(key)) {
-                                return <span>
-                                    <a href={`tel:${value}`}
-                                       title='Phone'
-                                       className='ml-3'
-                                       key={value}>
-                                    {value}
-                                </a>
-                                </span>
-                            }
-
-                            if (email.includes(key)) {
-                                return <span>
-                                    <a href={`mailto:${value}`}
-                                       title='Email'
-                                       className='ml-3'
-                                       key={value}>
-                                    {value}
-                                </a>
-                                </span>
-                            }
-
-                            return null;
-                        })
-                    }
-                </div>
-                <div className='mt-3'>
-                    {
-                        Object.entries(links).map(([key, value], index) => {
-                            const className = cx('', {'mt-3': index !== 0})
-                            return smallSize ? <div className={className}>
-                                <Link to={`/${language}/${key}`} title={value} key={key}
-                                      onClick={scrollToTop}>
-                                    {value}
-                                </Link>
-                            </div> : <Link to={`/${language}/${key}`} title={value} key={key} className='col-2'
-                                           onClick={scrollToTop}>
-                                {value}
-                            </Link>
-                        })
-                    }
+            <div className={`section ${isHydrated ? '' : ' not-hydrated'}`}>
+                <div className={`text-center ${isHydrated ? '' : ' not-hydrated'}`}>
+                    <span>{address.name}</span>
+                    <div className={`mt-3 text-container ${isHydrated ? '' : ' not-hydrated'}`}>
+                        {
+                            Object.entries(links).map(([key, value], index) => {
+                                const className = cx('', { 'mt-3': index !== 0 });
+                                return <div className={className} key={key}>
+                                    <Link to={`/${language}/${key}`} title={value} onClick={scrollToTop}>
+                                        {value}
+                                    </Link>
+                                </div>;
+                            })
+                        }
+                    </div>
                 </div>
             </div>
-            <div className='mt-3 mb-3 w-100'>
-                <Icons iconClassName='col-lg-1 col-md-1 col-3' className="d-flex flex-wrap align-center-full"/>
+            <div className={`section ${isHydrated ? '' : ' not-hydrated'}`}>
+                <div className={`text-center ${isHydrated ? '' : ' not-hydrated'}`}>
+                    <span>{contactsName}</span>
+                    <div className='mt-3'>
+                        <span>{address.address}</span>
+                    </div>
+                    <div className='mt-3'>
+                        {
+                            Object.entries(contacts).map(([key, value]) => {
+                                const phoneNumber = ['phoneNumber', 'extraPhoneNumber'];
+                                const email = ['email'];
+
+                                if (phoneNumber.includes(key)) {
+                                    return <span key={value}>
+                                        <a href={`tel:${value}`} title='Phone' className='ml-3'>
+                                            {value}
+                                        </a>
+                                    </span>;
+                                }
+
+                                if (email.includes(key)) {
+                                    return <span key={value}>
+                                        <a href={`mailto:${value}`} title='Email' className='ml-3'>
+                                            {value}
+                                        </a>
+                                    </span>;
+                                }
+
+                                return null;
+                            })
+                        }
+                    </div>
+                    <div className='mt-3 mb-3'>
+                        <Icons iconClassName='ml-5'
+                               className="d-flex flex-wrap align-center-full"/>
+                    </div>
+                </div>
+            </div>
+            <div className={`section ${isHydrated ? '' : ' not-hydrated'}`}>
+                <div className={`text-center ${isHydrated ? '' : ' not-hydrated'}`}>
+                    <div className='mt-3 icon-container'>
+                        <img className='header-icon' src={visaIcon} title='Visa' alt='Visa' loading='lazy' />
+                        <img className='header-icon' src={masterCardIcon} title='MasterCard' alt='MasterCard' loading='lazy' />
+                    </div>
+                </div>
             </div>
             <div className='politics d-flex justify-content-center'>{text}</div>
         </div>
--- src/pages/header/components/socialContacts/Icons.tsx
+++ src/pages/header/components/socialContacts/Icons.tsx
@@ -3,20 +3,25 @@ import viber from '../socialContacts/images/viber.svg';
 import telegram from '../socialContacts/images/telegram.png';
 import whatsapp from '../socialContacts/images/whatsapp.png';
 
-export const Icons: React.FC<IIcons> = ({className, iconClassName}) => {
+export const Icons = ({ className, iconClassName }: IIcons) => {
     return (
-        <div className={className}>
-            <a className={iconClassName} target='_blank' rel='noopener noreferrer' href='viber://chat?number=380676951098' title={'Viber'}>
-                <img className='header-icon-first' src={viber}  title={'Viber'} alt={'Viber'} loading='lazy'/>
-            </a>
-            <a href='https://t.me/Sitam_LLC' target='_blank' rel='noopener noreferrer' className={iconClassName} title={'Telegram'}>
-                <img className='header-icon' src={telegram} title={'Telegram'} alt={'Telegram'} loading='lazy'/>
-            </a>
-            <a className={iconClassName} target='_blank' rel='noopener noreferrer' href='https://wa.me/+380676951098?text=Доброго дня,%20я%20стосовно' title={'Whatsapp'} >
-                <img className='header-icon' src={whatsapp} title={'Whatsapp'} alt={'Whatsapp'} loading='lazy'/>
-            </a>
-        </div>
-    )
+        <>
+            <div className={className}>
+                <a className={iconClassName} target='_blank' rel='noopener noreferrer'
+                   href='viber://chat?number=380676951098' title={'Viber'}>
+                    <img className='header-icon-first' src={viber} title={'Viber'} alt={'Viber'} loading='lazy' />
+                </a>
+                <a href='https://t.me/Sitam_LLC' target='_blank' rel='noopener noreferrer' className={iconClassName}
+                   title={'Telegram'}>
+                    <img className='header-icon' src={telegram} title={'Telegram'} alt={'Telegram'} loading='lazy' />
+                </a>
+                <a className={iconClassName} target='_blank' rel='noopener noreferrer'
+                   href='https://wa.me/+380676951098?text=Доброго дня,%20я%20стосовно' title={'Whatsapp'}>
+                    <img className='header-icon' src={whatsapp} title={'Whatsapp'} alt={'Whatsapp'} loading='lazy' />
+                </a>
+            </div>
+        </>
+    );
 };
 
 interface IIcons {
